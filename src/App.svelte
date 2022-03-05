3cb675e29c1903a2154002c0a6432cbfcc291f60
Tao Chen <xtc2008@gmail.com>
2022-03-05 09:36:20 -0500

    created bar chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,12 @@
 <!--Load the AJAX API-->
 <script>
-  import ColumnChart from "../src/ColumnChart.svelte";
-  // Load the Visualization API and the corechart package.
-  google.charts.load("current", { packages: ["corechart"] });
+  import BarChart2 from "./BarChart2.svelte";
 
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn("string", "Topping");
-    data.addColumn("number", "Slices");
-    data.addRows([
-      ["Mushrooms", 3],
-      ["Onions", 1],
-      ["Olives", 1],
-      ["Zucchini", 1],
-      ["Pepperoni", 2],
-    ]);
-
-    // Set chart options
-    var options = {
-      title: "How Much Pizza I Ate Last Night",
-      width: 400,
-      height: 300,
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(
-      document.getElementById("chart_div")
-    );
-    chart.draw(data, options);
-  }
-  const popData = [
-    {
-      Country: "United States",
-      Population: "12394",
-    },
-    {
-      Country: "Russia",
-      Population: "6148",
-    },
-    {
-      Country: "Germany (FRG)",
-      Population: "1653",
-    },
-    {
-      Country: "France",
-      Population: "2162",
-    },
-    {
-      Country: "United Kingdom",
-      Population: "1214",
-    },
-    {
-      Country: "China",
-      Population: "1131",
-    },
-    {
-      Country: "Spain",
-      Population: "814",
-    },
-    {
-      Country: "Netherlands",
-      Population: "1167",
-    },
-    {
-      Country: "Italy",
-      Population: "660",
-    },
-    {
-      Country: "Israel",
-      Population: "1263",
-    },
-  ];
 </script>
 
 <body>
   <!--Div that will hold the pie chart-->
-  <ColumnChart data={popData} />
-  <div id="chart_div" />
+  <div>
+    <BarChart2/>
+  </div>
 </body>
