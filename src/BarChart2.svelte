30302df7c659ecc1a0104528d0fbcca2b237e1ba
Tao Chen <xtc2008@gmail.com>
2022-03-07 10:46:22 -0500

    barchat2

--- src/BarChart2.svelte
+++ src/BarChart2.svelte
@@ -1,144 +1,158 @@
 <script>
   import * as d3 from "d3";
   const alphabet = [
-    {"letter":"A","frequency":0.08167},
-    {"letter":"B","frequency":0.01492},
-    {"letter":"C","frequency":0.02782},
-    {"letter":"D","frequency":0.04253},
-    {"letter":"E","frequency":0.12702},
-    {"letter":"F","frequency":0.02288},
-    {"letter":"G","frequency":0.02015},
-    {"letter":"H","frequency":0.06094},
-    {"letter":"I","frequency":0.06966},
-    {"letter":"J","frequency":0.00153},
-    {"letter":"K","frequency":0.00772},
-    {"letter":"L","frequency":0.04025},
-    {"letter":"M","frequency":0.02406},
-    {"letter":"N","frequency":0.06749},
-    {"letter":"O","frequency":0.07507},
-    {"letter":"P","frequency":0.01929},
-    {"letter":"Q","frequency":0.00095},
-    {"letter":"R","frequency":0.05987},
-    {"letter":"S","frequency":0.06327},
-    {"letter":"T","frequency":0.09056},
-    {"letter":"U","frequency":0.02758},
-    {"letter":"V","frequency":0.00978},
-    {"letter":"W","frequency":0.0236},
-    {"letter":"X","frequency":0.0015},
-    {"letter":"Y","frequency":0.01974},
-    {"letter":"Z","frequency":0.00074}
-  ]
+    { letter: "A", frequency: 0.08167 },
+    { letter: "B", frequency: 0.01492 },
+    { letter: "C", frequency: 0.02782 },
+    { letter: "D", frequency: 0.04253 },
+    { letter: "E", frequency: 0.12702 },
+    { letter: "F", frequency: 0.02288 },
+    { letter: "G", frequency: 0.02015 },
+    { letter: "H", frequency: 0.06094 },
+    { letter: "I", frequency: 0.06966 },
+    { letter: "J", frequency: 0.00153 },
+    { letter: "K", frequency: 0.00772 },
+    { letter: "L", frequency: 0.04025 },
+    { letter: "M", frequency: 0.02406 },
+    { letter: "N", frequency: 0.06749 },
+    { letter: "O", frequency: 0.07507 },
+    { letter: "P", frequency: 0.01929 },
+    { letter: "Q", frequency: 0.00095 },
+    { letter: "R", frequency: 0.05987 },
+    { letter: "S", frequency: 0.06327 },
+    { letter: "T", frequency: 0.09056 },
+    { letter: "U", frequency: 0.02758 },
+    { letter: "V", frequency: 0.00978 },
+    { letter: "W", frequency: 0.0236 },
+    { letter: "X", frequency: 0.0015 },
+    { letter: "Y", frequency: 0.01974 },
+    { letter: "Z", frequency: 0.00074 },
+  ];
 
-  // Copyright 2021 Observable, Inc.
-  // Released under the ISC license.
-  // https://observablehq.com/@d3/bar-chart
-  function BarChart(data, {
-  x = (d, i) => i, // given d in data, returns the (ordinal) x-value
-  y = d => d, // given d in data, returns the (quantitative) y-value
-  title, // given d in data, returns the title text
-  marginTop = 20, // the top margin, in pixels
-  marginRight = 0, // the right margin, in pixels
-  marginBottom = 30, // the bottom margin, in pixels
-  marginLeft = 40, // the left margin, in pixels
-  width = 900, // the outer width of the chart, in pixels
-  height = 400, // the outer height of the chart, in pixels
-  xDomain, // an array of (ordinal) x-values
-  xRange = [marginLeft, width - marginRight], // [left, right]
-  yType = d3.scaleLinear, // y-scale type
-  yDomain, // [ymin, ymax]
-  yRange = [height - marginBottom, marginTop], // [bottom, top]
-  xPadding = 0.2, // amount of x-range to reserve to separate bars
-  yFormat, // a format specifier string for the y-axis
-  yLabel, // a label for the y-axis
-  color = "currentColor" // bar fill color
-} = {}) {
-  // Compute values.
-  const X = d3.map(data, x);
-  const Y = d3.map(data, y);
+  let data = alphabet;
+  let marginTop = 20; // the top margin, in pixels
+  let marginRight = 0; // the right margin, in pixels
+  let marginBottom = 30; // the bottom margin, in pixels
+  let marginLeft = 40; // the left margin, in pixels
+  let width = 900; // the outer width of the chart, in pixels
+  let height = 600; // the outer height of the chart, in pixels
+  let xPadding = 0.2; // amount of x-range to reserve to separate bars
+  let yFormat = "%"; // a format specifier string for the y-axis
+  let yLabel = "↑ Frequency"; // a label for the y-axis
+  let color = "steelblue"; // bar fill color
+  let yScalefactor = 6; //y-axis number of values
+
+  // Compute values X and Y value of Arrays
+  let x = Object.keys(data[0])[0]; // given d in data, returns the (ordinal) x-value
+  let y = Object.keys(data[0])[1]; // given d in data, returns the (quantitative) y-value
+  const X = data.map((el) => el[x]);
+  const Y = data.map((el) => el[y]);
 
   // Compute default domains, and unique the x-domain.
+  let xDomain; // an array of (ordinal) x-values // sort by descending frequency
+  let yDomain; // [ymin, ymax]
   if (xDomain === undefined) xDomain = X;
   if (yDomain === undefined) yDomain = [0, d3.max(Y)];
   xDomain = new d3.InternSet(xDomain);
 
   // Omit any data not present in the x-domain.
-  const I = d3.range(X.length).filter(i => xDomain.has(X[i]));
+  const I = d3.range(X.length).filter((i) => xDomain.has(X[i]));
 
   // Construct scales, axes, and formats.
+  let xRange = [marginLeft, width - marginRight]; // [left, right]
+  let yRange = [height - marginBottom, marginTop]; // [bottom, top]
+  let yType = d3.scaleLinear; // y-scale type
   const xScale = d3.scaleBand(xDomain, xRange).padding(xPadding);
   const yScale = yType(yDomain, yRange);
-  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
-  const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
 
-  // Compute titles.
-  if (title === undefined) {
-    const formatValue = yScale.tickFormat(100, yFormat);
-    title = i => `${X[i]}\n${formatValue(Y[i])}`;
-  } else {
-    const O = d3.map(data, d => d);
-    const T = title;
-    title = i => T(O[i], i, data);
+  let yTicks = [];
+  let unit = (Math.max(...Y) - Math.min(...Y)) / yScalefactor;
+  for (let i = 1; i < yScalefactor + 1; i++) {
+    yTicks.push(Math.floor(i * unit * 100));
   }
-
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-  svg.append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(yAxis)
-      .call(g => g.select(".domain").remove())
-      .call(g => g.selectAll(".tick line").clone()
-          .attr("x2", width - marginLeft - marginRight)
-          .attr("stroke-opacity", 0.1))
-      .call(g => g.append("text")
-          .attr("x", -marginLeft)
-          .attr("y", 10)
-          .attr("fill", "currentColor")
-          .attr("text-anchor", "start")
-          .text(yLabel));
-
-  const bar = svg.append("g")
-      .attr("fill", color)
-    .selectAll("rect")
-    .data(I)
-    .join("rect")
-      .attr("x", i => xScale(X[i]))
-      .attr("y", i => yScale(Y[i]))
-      .attr("height", i => yScale(0) - yScale(Y[i]))
-      .attr("width", xScale.bandwidth());
-
-  if (title) bar.append("title")
-      .text(title);
-
-  svg.append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(xAxis);
-
-  return svg.node();
-}
-
-let charts = BarChart(alphabet, {
-  x: d => d.letter,
-  y: d => d.frequency,
-  xDomain: d3.groupSort(alphabet, ([d]) => -d.frequency, d => d.letter), // sort by descending frequency
-  yFormat: "%",
-  yLabel: "↑ Frequency",
-  width: 600,
-  height: 500,
-  color: "steelblue"
-})
-
 </script>
 
-<div class='barChart'>
-  {@html charts.outerHTML}
-</div>
+<svg {width} {height} viewBox="0 0 {width} {height}">
+  <g class="y-axis" transform="translate({marginLeft}, 0)">
+    {#each yTicks as tick, i}
+      <g
+        class="tick"
+        opacity="1"
+        transform="translate(0, {yScale(tick / 100)})"
+      >
+        <line class="tick-start" stroke="black" stroke-opacity="1" x2="-6" />
+        <line class="tick-grid" x2={width - marginLeft - marginRight} />
+        <text x={-marginLeft} y="10">{tick + yFormat}</text>
+      </g>
+    {/each}
+    <text x="-{marginLeft}" y={marginTop}>{yLabel}</text>
+  </g>
+
+  <g class="bars">
+    {#each Y as point, i}
+      <rect
+        x={xScale(X[i])}
+        y={yScale(Y[i])}
+        width={xScale.bandwidth()}
+        height={yScale(0) - yScale(Y[i])}
+        fill={color}
+      />
+    {/each}
+  </g>
+
+  <g class="x-axis" transform="translate(0,{height - marginBottom})">
+    <path class="domain" stroke="black" d="M{marginLeft}, 0.5 H{width}" />
+    {#each X as point, i}
+      <g class="tick" opacity="1" transform="translate({xScale(point)},0)">
+        <line
+          x1={xScale.bandwidth() / 2}
+          x2={xScale.bandwidth() / 2}
+          stroke="black"
+          y2="6"
+        />
+        <text y={marginBottom} dx={xScale.bandwidth() / 4}>{X[i]}</text>
+      </g>
+    {/each}
+  </g>
+</svg>
 
 <style>
-  .barChart {
-    
+  svg {
+    max-width: 100%;
+    height: auto;
+    height: "intrinsic";
+  }
+
+  .y-axis {
+    font-size: "10px";
+    font-family: sans-serif;
+    text-anchor: "end";
+  }
+
+  .x-axis {
+    font-size: "10px";
+    font-family: sans-serif;
+    text-anchor: "end";
+  }
+
+  .tick {
+    opacity: 1;
+  }
+
+  .tick-start {
+    stroke: black;
+    stroke-opacity: 1;
+  }
+
+  .tick-grid {
+    stroke: black;
+    stroke-opacity: 0.2;
+    font-size: "11px";
+    color: black;
+  }
+
+  .tick text {
+    fill: black;
+    text-anchor: start;
   }
 </style>
