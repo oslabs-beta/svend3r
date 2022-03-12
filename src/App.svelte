a95c8f4e6acc64ada8c640e2aa61c5f3dabc000d
Isaac Saunders <IsaacLSaunders@gmail.com>
2022-03-11 19:51:11 -0500

    pull from dev

--- src/App.svelte
+++ src/App.svelte
@@ -1,134 +1,23 @@
 <!--Load the AJAX API-->
 <script>
-<<<<<<< HEAD
-  import BarChart2 from "./BarChart2.svelte";
-  import Bubble from "./Bubble.svelte"
-  // import ColumnChart from "../src/ColumnChart.svelte";
-<<<<<<< HEAD
-  import DoughnutChart from './DoughnutChart.svelte';
-  import ScatterOld from './Scatter-old.svelte';
-  import Scatter from './Scatter.svelte';
-=======
-  // import DoughnutChart from '../src/DoughnutChart.svelte'
-  // import AnimatedDoughnut from '../src/DoughnutAnim.svelte'
-  // Load the Visualization API and the corechart package.
-  google.charts.load("current", { packages: ["corechart"] });
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn("string", "Topping");
-    data.addColumn("number", "Slices");
-    data.addRows([
-      ["Mushrooms", 3],
-      ["Onions", 1],
-      ["Olives", 1],
-      ["Zucchini", 1],
-      ["Pepperoni", 2],
-    ]);
-
-    // Set chart options
-    var options = {
-      title: "How Much Pizza I Ate Last Night",
-      width: 400,
-      height: 300,
-      colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-      is3D: true,
-      pieSliceText: 'label',
-      slices: {
-              1: {offset: 0.2},
-              2: {offset: 0.3},
-              3: {offset: 0.4},
-              4: {offset: 0.5},
-      },
-      pieStartAngle: 210,
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(
-      document.getElementById("chart_div")
-    );
-    chart.draw(data, options);
-  }
-  const popData = [
-    {
-      Country: "United States",
-      Population: "12394",
-    },
-    {
-      Country: "Russia",
-      Population: "6148",
-    },
-    {
-      Country: "Germany (FRG)",
-      Population: "1653",
-    },
-    {
-      Country: "France",
-      Population: "2162",
-    },
-    {
-      Country: "United Kingdom",
-      Population: "1214",
-    },
-    {
-      Country: "China",
-      Population: "1131",
-    },
-    {
-      Country: "Spain",
-      Population: "814",
-    },
-    {
-      Country: "Netherlands",
-      Population: "1167",
-    },
-    {
-      Country: "Italy",
-      Population: "660",
-    },
-    {
-      Country: "Israel",
-      Population: "1263",
-    },
-  ];
-
-=======
   import BarChart2 from './BarChart2.svelte';
   import Scatter from './Scatter.svelte';
   import scatterdata from './nivo-scatter-data';
   import Chord from './Chord.svelte';
   import chordData from './observable-chord-data';
->>>>>>> 50c8ec469d0d74e06a2a8c0f6402a9f1aac8b00c
->>>>>>> main
+  import Bubble from './Bubble.svelte'
 </script>
 
 <body>
-  
+  <!-- Div that will hold the pie chart -->
+  <!-- <ColumnChart data={popData} />
+  <DoughnutChart/> -->
+  <!-- <AnimatedDoughnut/> -->4
+  <!-- <div id="chart_div" /> -->
   <div>
-<<<<<<< HEAD
-      <!-- <!-- <ColumnChart data={popData} /> -->
-      <!-- <Bubble/> -->
-    <!-- <DoughnutChart/> -->
-    <!-- <BarChart2/> -->
-    <Bubble/>
-    <!-- <ScatterOld/> -->
-    <!-- <Scatter/> -->
-=======
-<<<<<<< HEAD
-    <BarChart2/>
-    <Bubble/>
-=======
     <!-- <BarChart2/> -->
     <!-- <Scatter {scatterdata} /> -->
-    <Chord {chordData} />
->>>>>>> 50c8ec469d0d74e06a2a8c0f6402a9f1aac8b00c
->>>>>>> main
+    <!-- <Chord {chordData} /> -->
+    <Bubble/>
   </div>
 </body>
