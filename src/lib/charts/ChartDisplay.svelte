682cd561b7e885f2b272562d105257a196f58fbd
James Laff <jameslaff@gmail.com>
2022-03-17 14:25:52 -0400

    add bar and radial stacked charts

--- src/lib/charts/ChartDisplay.svelte
+++ src/lib/charts/ChartDisplay.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Area from './templates/Area.svelte';
-	// import Bar from './templates/Bar.svelte';
+	import Bar from './templates/Bar.svelte';
 	import Chord from './templates/Chord.svelte';
 	// import Choropleth from './templates/Choropleth.svelte';
 	// import CirclePack from './templates/CirclePack.svelte';
 	// import Line from './templates/Line.svelte';
 	// import Pie from './templates/Pie.svelte';
-	// import RadialStacked from './templates/RadialStacked.svelte';
+	import RadialStacked from './templates/RadialStacked.svelte';
 	// import Scatter from './templates/Scatter.svelte';
 
   export let slug,
@@ -16,13 +16,13 @@
 
   const chartComponents = {
     area: Area,
-    // bar: Bar,
+    bar: Bar,
     chord: Chord,
     // choropleth: Choropleth,
     // circlePack: CirclePack,
     // line: Line,
     // pie: Pie,
-    // radialStacked: RadialStacked,
+    radialStacked: RadialStacked,
     // scatter: Scatter
   }
 
