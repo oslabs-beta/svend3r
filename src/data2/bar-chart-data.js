0e9738ffc50dfe6d7b448ab98e8343708b4bdf46
calamitymike <calamitymike0@gmail.com>
2022-03-21 11:11:20 -0400

    TEST bar & pie chart data

---
+++ src/data2/bar-chart-data.js
@@ -0,0 +1,1028 @@
+let rawData = `Laravel;Symfony
+Angular;Flask;Vue.js
+Flask
+Angular;jQuery
+Flask
+Express;Flask;jQuery;React.js
+jQuery
+Angular.js;jQuery;Ruby on Rails
+Django;FastAPI
+NA
+Angular;ASP.NET Core 
+Express;React.js;Vue.js
+Ruby on Rails
+Flask
+Vue.js
+NA
+Angular;ASP.NET Core ;Spring
+jQuery;Laravel;Ruby on Rails
+NA
+Angular;Express;Flask;React.js;Svelte
+Spring
+Flask
+NA
+NA
+Angular;Express;Spring
+Django;jQuery
+NA
+NA
+NA
+NA
+Express;React.js;Svelte;Vue.js
+NA
+Angular.js;jQuery;Laravel
+jQuery
+NA
+NA
+NA
+NA
+React.js
+Express;jQuery;React.js
+NA
+NA
+Angular;Angular.js;React.js;Vue.js
+Django;Flask;React.js
+Django;Flask;Vue.js
+ASP.NET Core ;React.js
+NA
+NA
+NA
+Django;Express;React.js
+Angular;ASP.NET;ASP.NET Core ;Express;Svelte
+Angular.js;jQuery;React.js
+Angular;Express;React.js
+NA
+NA
+React.js;Spring
+NA
+Angular;ASP.NET Core ;jQuery;Spring
+Django;FastAPI;Vue.js
+Django;Flask
+jQuery;Laravel;Svelte;Vue.js
+NA
+Express
+NA
+Flask
+Laravel
+Django;FastAPI;Flask
+ASP.NET
+NA
+Express;Gatsby;React.js
+NA
+jQuery
+jQuery;React.js
+NA
+Angular;Express;Gatsby;React.js;Spring;Vue.js
+jQuery;React.js
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Django;Express;Flask;jQuery;Laravel;React.js;Spring;Vue.js
+Flask
+Spring
+Angular;Django;FastAPI;Flask
+jQuery
+NA
+jQuery;Laravel;React.js;Vue.js
+ASP.NET;ASP.NET Core ;jQuery;React.js
+Angular;Django;Express;jQuery;React.js;Vue.js
+Django;Express;Flask;jQuery;Spring;Vue.js
+ASP.NET Core 
+NA
+jQuery
+Flask;jQuery;Spring
+Django;Flask;React.js
+ASP.NET;ASP.NET Core ;jQuery;React.js
+Angular;Express;Symfony;Vue.js
+NA
+NA
+Angular;ASP.NET;ASP.NET Core ;jQuery
+React.js
+Laravel;React.js
+NA
+NA
+Django;Vue.js
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery
+Django
+NA
+Angular;Laravel;Vue.js
+Angular;ASP.NET;ASP.NET Core ;jQuery
+React.js
+React.js
+jQuery;Spring
+Angular.js;Django;React.js
+ASP.NET;ASP.NET Core ;Express;Flask;React.js
+NA
+React.js
+Angular;Laravel;Symfony
+jQuery;React.js;Vue.js
+NA
+Angular;React.js;Spring
+Express;React.js
+Angular.js;ASP.NET;ASP.NET Core ;jQuery
+React.js
+NA
+Express;React.js
+jQuery;Laravel;React.js;Symfony;Vue.js
+NA
+NA
+NA
+Django;Express;jQuery;Laravel;React.js
+NA
+ASP.NET;Vue.js
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery;Vue.js
+Angular;jQuery;Laravel;React.js;Symfony
+NA
+NA
+NA
+React.js
+Express;Flask;Laravel;React.js;Vue.js
+ASP.NET;ASP.NET Core ;Django
+NA
+Django;FastAPI;Flask;Gatsby;jQuery;React.js
+Angular;ASP.NET Core 
+Flask;jQuery;Ruby on Rails;Spring
+NA
+React.js
+NA
+Django;jQuery
+React.js
+NA
+NA
+jQuery
+React.js
+Angular;Express;Laravel
+NA
+ASP.NET;ASP.NET Core ;jQuery
+Django;FastAPI;Flask;jQuery;React.js
+Angular;Express;Gatsby;jQuery;React.js
+ASP.NET
+Django;jQuery
+React.js
+jQuery;React.js;Ruby on Rails
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery
+NA
+NA
+NA
+ASP.NET Core ;React.js
+FastAPI;React.js;Svelte;Vue.js
+ASP.NET Core 
+React.js;Symfony
+Angular;Express;jQuery;React.js;Spring;Vue.js
+Flask
+Express;Flask
+NA
+jQuery;Laravel;Symfony;Vue.js
+Spring
+Express;React.js
+Angular;Spring
+NA
+NA
+ASP.NET;ASP.NET Core 
+NA
+jQuery;Spring
+NA
+NA
+Django;Flask
+ASP.NET;ASP.NET Core ;jQuery
+jQuery;React.js;Symfony;Vue.js
+Angular;Spring
+Angular;Express;React.js;Vue.js
+ASP.NET;ASP.NET Core ;jQuery;Vue.js
+Angular;Angular.js;jQuery;React.js
+Django;jQuery
+Flask;React.js
+Django;Express;jQuery;React.js
+Angular;React.js;Ruby on Rails
+NA
+ASP.NET;ASP.NET Core 
+Django
+NA
+ASP.NET;Django;jQuery;Laravel;React.js
+Flask
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery
+Express
+Angular;ASP.NET;ASP.NET Core ;jQuery;React.js
+Django;Flask;jQuery
+ASP.NET;ASP.NET Core 
+Express;React.js
+Angular;Angular.js;Django;jQuery;Laravel;React.js;Symfony
+NA
+Spring
+Express;React.js
+Django;Flask
+NA
+Express;React.js
+NA
+ASP.NET;ASP.NET Core ;FastAPI;Flask;React.js
+NA
+NA
+Angular;ASP.NET Core ;Express;jQuery
+NA
+Flask
+Django;React.js
+Angular
+Django
+Express;FastAPI;Flask;Ruby on Rails
+Express;Gatsby;React.js
+NA
+ASP.NET;ASP.NET Core ;jQuery;Vue.js
+NA
+Angular;Spring;Vue.js
+ASP.NET Core ;Vue.js
+jQuery;Laravel;React.js
+React.js
+Flask
+NA
+Express;Gatsby;React.js;Vue.js
+Angular.js;jQuery;React.js;Ruby on Rails;Spring;Vue.js
+Vue.js
+React.js
+Express;React.js
+Vue.js
+NA
+Angular;ASP.NET;ASP.NET Core ;jQuery
+Angular.js;Express;jQuery;Laravel;React.js;Ruby on Rails;Symfony;Vue.js
+Spring
+Angular;ASP.NET;ASP.NET Core 
+NA
+NA
+NA
+NA
+Django;Flask;jQuery
+Django;React.js
+Flask
+NA
+NA
+Angular;Angular.js;ASP.NET;ASP.NET Core 
+jQuery;Laravel
+Django;Express;React.js
+ASP.NET;ASP.NET Core 
+jQuery;React.js
+NA
+React.js;Vue.js
+jQuery;Vue.js
+NA
+Gatsby;React.js
+Angular;Angular.js;Drupal;Express;jQuery;Laravel;Symfony
+ASP.NET Core ;React.js
+Django;Flask;jQuery;Vue.js
+Angular;ASP.NET Core ;Django;jQuery;Spring;Vue.js
+Express;jQuery;Spring
+Flask;Vue.js
+Express;jQuery;React.js;Spring;Symfony;Vue.js
+Express;Laravel;Vue.js
+Angular;Express;React.js;Symfony
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Django;Express;Flask;jQuery;Laravel;React.js;Ruby on Rails;Spring;Symfony;Vue.js
+React.js
+NA
+ASP.NET;ASP.NET Core ;jQuery
+Angular
+Django;Flask
+NA
+NA
+Angular;Angular.js;ASP.NET;jQuery;React.js
+jQuery
+Angular;Angular.js;Django;jQuery
+NA
+Django;jQuery
+NA
+Angular.js;ASP.NET;jQuery;Spring
+ASP.NET Core ;jQuery
+Angular;Angular.js;jQuery;Spring
+jQuery;React.js;Ruby on Rails
+jQuery;Laravel;React.js;Vue.js
+NA
+Express;React.js
+NA
+NA
+React.js
+Angular
+Django;Express;Gatsby;jQuery;React.js;Vue.js
+Laravel
+NA
+Angular;Django
+Angular;Django;Drupal;jQuery;Laravel;React.js;Spring
+NA
+Django;jQuery
+Express;React.js;Vue.js
+Angular;React.js
+Flask
+Express
+React.js;Vue.js
+NA
+Laravel;React.js
+Angular;Django;React.js;Spring
+Flask
+React.js
+jQuery
+Laravel;Symfony
+NA
+Django;Express;Flask;jQuery;React.js;Ruby on Rails
+Angular;ASP.NET Core 
+Express;React.js
+Flask
+jQuery
+Django;Flask;React.js
+NA
+Angular;Angular.js;Express;React.js;Spring
+NA
+NA
+NA
+ASP.NET;jQuery
+ASP.NET;Laravel
+Drupal;jQuery
+Django
+NA
+Angular.js;Django;Vue.js
+NA
+Spring
+NA
+Django
+Drupal;Laravel;Symfony;Vue.js
+NA
+Django
+NA
+NA
+Django;Express;Flask;React.js;Vue.js
+Angular;ASP.NET;ASP.NET Core ;FastAPI;Flask;Vue.js
+NA
+Express;React.js;Vue.js
+ASP.NET;ASP.NET Core ;Express;jQuery;React.js
+React.js
+ASP.NET;ASP.NET Core ;jQuery
+Flask
+jQuery;Laravel;Ruby on Rails;Vue.js
+ASP.NET
+Flask
+NA
+NA
+Angular;ASP.NET;ASP.NET Core 
+ASP.NET;ASP.NET Core ;jQuery
+NA
+NA
+React.js
+Flask;React.js
+NA
+ASP.NET Core 
+NA
+Angular;Django;React.js
+Laravel;React.js;Symfony;Vue.js
+React.js;Ruby on Rails;Spring
+NA
+React.js;Spring
+NA
+NA
+FastAPI
+Vue.js
+NA
+Express;React.js
+Django;jQuery
+jQuery
+Angular;Angular.js
+Django;React.js
+ASP.NET Core ;Vue.js
+NA
+Express;Gatsby;jQuery;React.js
+Angular.js;jQuery
+NA
+NA
+NA
+Angular;Angular.js;jQuery;Laravel;Spring
+Express;jQuery;React.js
+Django;jQuery
+NA
+Flask;jQuery
+Flask;jQuery;Vue.js
+NA
+Django;React.js
+NA
+jQuery;Symfony
+NA
+Vue.js
+Angular;Express;React.js;Symfony
+jQuery
+Express;React.js;Vue.js
+NA
+ASP.NET;Flask;jQuery
+NA
+ASP.NET;ASP.NET Core 
+ASP.NET Core 
+NA
+NA
+NA
+NA
+Flask
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Django;Express;jQuery;Laravel;React.js
+Angular;ASP.NET;ASP.NET Core ;jQuery
+ASP.NET Core ;jQuery
+Spring
+NA
+Angular;React.js
+Angular;Django;Flask;React.js
+NA
+jQuery;Spring
+NA
+ASP.NET;ASP.NET Core ;jQuery
+NA
+NA
+React.js
+NA
+Django
+NA
+Angular;Spring
+NA
+Django;Flask;jQuery;Symfony
+NA
+ASP.NET;ASP.NET Core ;Django;React.js
+Django;Flask
+NA
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Spring
+NA
+Spring
+Flask
+Django;Flask
+Angular;Express
+Angular;jQuery
+NA
+ASP.NET;jQuery
+Django
+Express;Gatsby;React.js
+NA
+React.js;Ruby on Rails
+Django;Flask
+NA
+ASP.NET;ASP.NET Core ;Flask;jQuery
+Angular;Angular.js
+NA
+React.js
+Express
+jQuery
+Django
+React.js
+React.js
+Angular;Spring
+Laravel
+Angular;Angular.js;Express;jQuery
+NA
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery;React.js;Spring;Vue.js
+Angular;Angular.js;Django;Spring
+ASP.NET Core 
+Angular
+Angular;Django;Express;FastAPI;Flask;React.js
+jQuery;React.js;Spring
+jQuery
+ASP.NET Core ;jQuery;React.js
+Angular;Spring
+Express;Vue.js
+Angular;ASP.NET;ASP.NET Core ;jQuery;Laravel;Spring;Vue.js
+Angular.js;Express;jQuery;React.js;Spring;Vue.js
+Angular;Angular.js;ASP.NET Core ;Express;React.js;Ruby on Rails;Vue.js
+NA
+jQuery;React.js;Spring
+Express;React.js
+ASP.NET Core 
+NA
+React.js;Vue.js
+ASP.NET;ASP.NET Core ;Vue.js
+NA
+Angular;ASP.NET;ASP.NET Core 
+Flask
+Flask;jQuery;Vue.js
+NA
+Angular;Angular.js;ASP.NET Core ;Express;React.js
+NA
+Flask
+Django;Spring
+ASP.NET;ASP.NET Core 
+Django;React.js
+Angular;Angular.js;Express;FastAPI;Gatsby;React.js;Svelte;Vue.js
+NA
+NA
+Angular.js
+React.js
+ASP.NET;jQuery;Vue.js
+jQuery;Laravel;Svelte;Vue.js
+NA
+NA
+ASP.NET
+React.js
+NA
+Django;Flask;jQuery;Vue.js
+NA
+Angular;ASP.NET;ASP.NET Core ;Express;jQuery;React.js
+Angular;Angular.js;jQuery;Laravel;React.js;Symfony;Vue.js
+Angular;Express;React.js
+React.js
+Express;jQuery;React.js;Spring
+NA
+jQuery;Laravel;Symfony
+NA
+FastAPI;Flask;jQuery
+Drupal;jQuery
+ASP.NET
+ASP.NET;ASP.NET Core 
+Angular;ASP.NET;ASP.NET Core 
+ASP.NET;ASP.NET Core ;jQuery
+Django;Express;Flask;Laravel;React.js;Spring
+Angular;Django;Express;React.js;Symfony
+Django;React.js
+NA
+Angular;React.js;Spring;Vue.js
+Express;React.js
+NA
+NA
+NA
+React.js
+NA
+Angular;Express;Gatsby;React.js
+Angular;Angular.js;jQuery
+React.js
+Django
+Angular;Angular.js;Django;Flask;jQuery
+jQuery;Spring
+NA
+Spring
+Express;jQuery
+Flask;React.js
+NA
+jQuery
+ASP.NET Core ;React.js
+Angular
+NA
+jQuery;Spring
+jQuery
+jQuery;Spring
+Flask;React.js
+Django;Flask;Svelte
+Express
+Django;React.js
+ASP.NET;ASP.NET Core 
+Django;jQuery
+Angular;ASP.NET Core ;Django;Flask
+ASP.NET;ASP.NET Core 
+React.js;Ruby on Rails
+Express;Flask;Vue.js
+ASP.NET;jQuery
+NA
+NA
+NA
+Spring
+Angular;Angular.js;Spring
+Express;jQuery;Laravel;Vue.js
+jQuery;React.js
+Angular;Angular.js;Django;Spring
+Angular;Flask;Spring
+NA
+ASP.NET Core ;jQuery
+Flask
+Express;Symfony
+Django;Flask
+React.js
+Express;Vue.js
+FastAPI;Flask
+Angular;Angular.js;Django;Express;Flask;jQuery;React.js;Ruby on Rails
+React.js;Vue.js
+Express
+Express;React.js
+Laravel;Vue.js
+NA
+ASP.NET
+NA
+NA
+ASP.NET;jQuery
+ASP.NET
+ASP.NET;ASP.NET Core ;jQuery
+NA
+NA
+jQuery
+Express;jQuery;React.js
+Angular;Express;Flask;React.js
+Flask
+ASP.NET Core ;jQuery;Laravel
+Flask
+ASP.NET Core ;Express;jQuery
+NA
+Angular;Angular.js;ASP.NET;Express;jQuery;React.js;Spring
+jQuery;React.js;Symfony
+Laravel
+NA
+Django;Flask;Vue.js
+NA
+NA
+ASP.NET;ASP.NET Core ;Express;jQuery;React.js;Vue.js
+Angular;Angular.js;Express;jQuery;Laravel;React.js;Vue.js
+Django
+Angular;Angular.js;ASP.NET;ASP.NET Core 
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery
+Angular;React.js
+Angular;Express;React.js;Vue.js
+Spring
+Angular;Express;Gatsby;React.js;Svelte;Vue.js
+NA
+NA
+NA
+jQuery;Symfony
+NA
+NA
+Express
+NA
+Django;React.js
+ASP.NET Core ;jQuery
+NA
+Express;React.js
+Express;React.js
+Angular;Express;Laravel;React.js
+NA
+Spring
+Django;Express;FastAPI;Flask;React.js
+ASP.NET;ASP.NET Core 
+Flask;React.js
+ASP.NET;ASP.NET Core ;Express;jQuery
+Angular
+React.js;Ruby on Rails
+Vue.js
+NA
+NA
+NA
+React.js;Ruby on Rails
+Angular;ASP.NET;jQuery
+Django;Express;FastAPI;Flask;React.js
+React.js
+NA
+Symfony
+ASP.NET;ASP.NET Core ;React.js
+ASP.NET Core ;jQuery
+ASP.NET Core 
+NA
+React.js
+Spring
+Express
+Laravel;Vue.js
+Angular;ASP.NET;ASP.NET Core ;jQuery
+NA
+Gatsby;Vue.js
+Gatsby;React.js;Vue.js
+Express;Laravel;React.js;Vue.js
+Angular.js;Gatsby;jQuery;React.js;Symfony
+NA
+NA
+React.js
+NA
+Express;React.js
+Gatsby;jQuery;Laravel;Vue.js
+ASP.NET;ASP.NET Core 
+Angular;Express
+NA
+ASP.NET;ASP.NET Core ;jQuery;React.js
+NA
+Express;Vue.js
+NA
+Angular;Spring;Symfony
+NA
+Ruby on Rails
+NA
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Django;Drupal;Express;jQuery;React.js;Spring;Symfony;Vue.js
+NA
+jQuery
+NA
+NA
+NA
+Angular.js;ASP.NET;jQuery;Vue.js
+ASP.NET;ASP.NET Core ;jQuery;React.js
+NA
+ASP.NET
+Express;jQuery;Laravel;React.js;Vue.js
+NA
+Spring
+Angular;jQuery;React.js;Symfony
+Express;jQuery;React.js
+Express;Gatsby;jQuery;React.js;Ruby on Rails
+ASP.NET;ASP.NET Core ;Vue.js
+NA
+NA
+Spring
+Angular;Express;React.js;Ruby on Rails;Spring
+ASP.NET;ASP.NET Core 
+ASP.NET
+NA
+Express;Laravel
+NA
+NA
+Express;Gatsby;Laravel;React.js;Vue.js
+NA
+ASP.NET Core ;Django;Express;React.js;Vue.js
+Vue.js
+jQuery;Laravel;Symfony;Vue.js
+Angular;Express;jQuery;Laravel
+Express;React.js;Spring
+Angular;Django;Spring
+ASP.NET;ASP.NET Core ;jQuery;Vue.js
+jQuery;Spring
+ASP.NET;jQuery;Vue.js
+Express;jQuery;Laravel;Symfony;Vue.js
+Django;Express;React.js
+Django;Flask
+Django;React.js
+Angular;Angular.js;Django;Express;Flask;jQuery;Laravel;React.js;Vue.js
+Angular;Vue.js
+Flask
+ASP.NET;ASP.NET Core 
+Spring
+NA
+Django;jQuery
+NA
+Angular;ASP.NET
+NA
+NA
+Angular;FastAPI
+Angular;Angular.js;Spring
+NA
+Flask;jQuery
+NA
+Express
+Angular;Django
+ASP.NET Core ;Express;Vue.js
+Flask;jQuery
+Angular;Angular.js;ASP.NET;ASP.NET Core ;jQuery;React.js
+Angular.js;Spring
+Django;Express;Flask;jQuery;Laravel;React.js
+Express;Gatsby;jQuery;React.js;Svelte
+Angular;ASP.NET Core ;Express;jQuery;React.js;Vue.js
+Django
+NA
+ASP.NET;ASP.NET Core 
+Angular;Angular.js;ASP.NET;ASP.NET Core ;Express;jQuery
+ASP.NET;ASP.NET Core ;Express;Spring
+NA
+NA
+NA
+NA
+Express;React.js;Spring
+jQuery
+NA
+Angular;Vue.js
+ASP.NET;ASP.NET Core 
+Laravel;Ruby on Rails
+Express;React.js;Spring
+NA
+NA
+NA
+NA
+Express;React.js;Svelte
+jQuery
+Django;jQuery;Laravel;React.js
+Express;FastAPI;jQuery
+ASP.NET Core ;jQuery
+ASP.NET;jQuery;Vue.js
+ASP.NET Core 
+ASP.NET;ASP.NET Core ;jQuery
+jQuery;Laravel;Vue.js
+Angular
+Spring
+jQuery;Laravel;Vue.js
+Express;jQuery;Laravel;Vue.js
+Angular;Express;jQuery;React.js
+Django
+NA
+Angular;Flask
+Django;Flask;Ruby on Rails;Vue.js
+NA
+jQuery;React.js;Vue.js
+Angular;Angular.js;jQuery
+NA
+Express;Laravel;Vue.js
+Express
+NA
+NA
+ASP.NET;ASP.NET Core ;jQuery;React.js
+NA
+Angular;ASP.NET;jQuery;Spring
+NA
+NA
+NA
+NA
+Express;React.js
+NA
+NA
+Django;Flask
+ASP.NET Core ;jQuery
+Django;Express;React.js
+ASP.NET Core ;React.js
+Angular;Angular.js;ASP.NET;Django;Drupal;Express;FastAPI;Flask;jQuery;React.js;Vue.js
+Flask;Vue.js
+Django;Flask;jQuery
+Express;React.js
+NA
+NA
+jQuery;React.js;Vue.js
+Gatsby;jQuery;React.js
+Gatsby;jQuery;Laravel;React.js
+NA
+React.js
+Gatsby;jQuery;React.js;Spring
+Express;Laravel;React.js;Vue.js
+Ruby on Rails
+NA
+NA
+Django;Flask;Vue.js
+NA
+NA
+NA
+React.js
+Django;Express;Gatsby;React.js
+Drupal;jQuery;React.js;Svelte;Symfony
+Laravel;Vue.js
+Drupal;Express;Flask;jQuery;Laravel;React.js;Vue.js
+Django;Flask;jQuery;React.js
+NA
+NA
+ASP.NET;jQuery
+NA
+Django;Flask;jQuery
+ASP.NET;ASP.NET Core ;React.js
+Express;jQuery;Laravel;Vue.js
+NA
+Flask
+NA
+NA
+Express;React.js
+Flask
+Express;jQuery;Ruby on Rails;Spring;Vue.js
+NA
+Angular;Angular.js;Spring
+jQuery
+jQuery;React.js;Vue.js
+ASP.NET;ASP.NET Core 
+NA
+NA
+jQuery
+Angular;Express
+Drupal;jQuery;React.js
+jQuery
+NA
+NA
+Angular.js;jQuery
+Angular;jQuery
+Flask;Spring
+React.js
+NA
+NA
+jQuery
+Angular;Express
+NA
+Angular.js;ASP.NET;ASP.NET Core ;jQuery;Spring
+NA
+Express;Vue.js
+Angular;Gatsby;React.js
+Angular;Angular.js;Express;jQuery;Vue.js
+ASP.NET;Drupal;Ruby on Rails
+Angular;ASP.NET;ASP.NET Core ;Vue.js
+Angular;ASP.NET;ASP.NET Core ;Flask
+Express;jQuery;React.js
+Express;React.js;Vue.js
+jQuery
+ASP.NET;jQuery
+Django;Flask;jQuery
+React.js
+Django;Drupal;jQuery;Symfony
+Express;React.js
+Angular.js;Django;Express;jQuery;React.js;Vue.js
+NA
+Angular;jQuery;Spring;Vue.js
+Angular;Angular.js;Drupal;jQuery;Laravel
+NA
+React.js;Spring
+Express;jQuery;React.js
+Spring
+Angular;ASP.NET Core 
+Angular;Angular.js;Spring
+React.js;Spring;Vue.js
+NA
+NA
+Angular;Django;Laravel
+Angular
+jQuery;Vue.js
+NA
+Django;FastAPI;Spring
+Express;React.js;Svelte
+Angular;jQuery
+NA
+NA
+NA
+Express;Laravel
+NA
+Angular;Angular.js;Flask;jQuery;React.js;Vue.js
+Angular
+FastAPI;Flask;Symfony
+NA
+NA
+NA
+Spring
+NA
+Angular;Express;React.js;Vue.js
+Angular;Express;React.js
+jQuery;Laravel
+Vue.js
+NA
+NA
+Spring;Vue.js
+Angular;Express;Spring
+Express;jQuery
+NA
+NA
+NA
+Express;jQuery
+Django
+NA
+NA
+Angular;jQuery;Spring
+React.js
+ASP.NET;ASP.NET Core ;Django;Express;jQuery;Laravel;React.js
+NA
+NA
+Angular;Angular.js;Spring
+Vue.js
+Angular.js;ASP.NET Core ;jQuery;Vue.js
+Express;Gatsby;jQuery;React.js;Vue.js
+Angular;Express;jQuery;Spring
+NA
+jQuery;Symfony
+Express;Gatsby;React.js;Vue.js
+Angular;Drupal;jQuery;Spring
+FastAPI;Flask
+Angular;Spring
+NA
+Angular;Angular.js;Laravel
+Angular;Angular.js;jQuery
+ASP.NET;ASP.NET Core ;Flask;jQuery
+NA
+Angular;jQuery
+Django;Express;Gatsby;React.js
+Express;Gatsby;jQuery;React.js;Svelte;Vue.js
+Angular;React.js
+Express;jQuery
+Django
+NA
+Angular;ASP.NET;ASP.NET Core 
+Flask
+Django;jQuery
+Express
+NA
+Angular
+Angular;Angular.js;Express
+Django
+NA
+Flask
+NA
+NA
+React.js
+Express;FastAPI;React.js
+React.js;Spring
+React.js
+Angular;Angular.js;Express;Flask
+Flask;React.js
+Flask;jQuery;React.js
+React.js
+NA
+Express;React.js
+NA
+Django;jQuery
+Express;Vue.js
+NA
+Django;Vue.js
+Angular;jQuery;Spring;Symfony
+jQuery
+Angular;Express
+jQuery
+Express;FastAPI;Flask;jQuery;React.js
+Laravel;React.js;Svelte;Vue.js
+React.js
+jQuery;Svelte
+NA`;
+
+
+
+// const sampleData = [
+//   { letter: 'A', frequency: 0.08167 }
+// ]
+
+const frameworkUsage = {}
+rawData = rawData.split('\n');
+rawData.forEach(ele => {
+  ele = ele.split(';')
+  ele.forEach(ele => {
+    if (frameworkUsage.hasOwnProperty(ele)) frameworkUsage[ele] += 1;
+    else frameworkUsage[ele] = 1;
+  })
+})
+
+const data = [];
+
+for (const [key, value] of Object.entries(frameworkUsage)) {
+  //console.log(`${key}: ${value}`);
+  data.push({ framework: key, numberOfUsers: value })
+}
+
+// console.table(frameworkUsage)
+// console.table(data);
+// console.table(data)
+
+export { data };
