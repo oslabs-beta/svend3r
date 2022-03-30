b2a8268cddb7ef77a755526f95d0c34c1376a0e6
James Laff <jameslaff@gmail.com>
2022-03-30 16:22:40 -0400

    revert to placeholder data

---
+++ sandbox/lib-old/charts/data/bar-data-housing.js
@@ -0,0 +1,14 @@
+export default [
+  { month: 'Jan', cost: 86400 },
+  { month: 'Feb', cost: 83400 },
+  { month: 'Mar', cost: 82900 },
+  { month: 'Apr', cost: 86900 },
+  { month: 'May', cost: 85900 },
+  { month: 'Jun', cost: 85600 },
+  { month: 'Jul', cost: 84200 },
+  { month: 'Aug', cost: 88200 },
+  { month: 'Sep', cost: 87300 },
+  { month: 'Oct', cost: 89400 },
+  { month: 'Nov', cost: 92600 },
+  { month: 'Dec', cost: 90200 }
+];
